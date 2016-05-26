7506b67a6fa30787bf44c03e03a6c70c7667e07a
master1312 <master1312@yandex.ru>
2016-05-27 02:00:44 +0300

    * fix

--- 3rdparty/update.sh
+++ 3rdparty/update.sh
@@ -11,81 +11,76 @@ $updaterScript update dir [-r] - update one dependency"
 
 cd `dirname $0`
 
-if [[ "$1" = "--help" || -z "$1" ]]; then
+if [[ "$1" = "--help" ]]; then
 	echo -e "$helpStr"
 	exit 0
 fi
 
-case "$1" in
-	all)
-		for dir in *
-		do
-			if [[ "$dir" = "." || "$dir" = ".." ]];then continue; fi
-			if [[ ! -d "$dir" ]]; then continue; fi
-			echo "Updating: $dir"
-			cd "$dir"
-			git pull
-			if [ -x "./3rdparty/$updaterScript" ];then
-				echo "Updating $dir/3rdparty"
-				"./3rdparty/$updaterScript" all
-			fi
-		cd - > /dev/null
-		done
-		;;
-	add)
-		if [[ -z "$2" || -z "$3" ]]; then
-			echo -e "$helpStr";
-			exit 1
-		fi
-		url=$2
-		dir=$3
-		if [[ -e "$dir" ]]; then
-			echo "Dir $dir already exists"
-			exit 1
-		fi
-		git clone "$url" "$dir"
-		if [ -x "./$dir/3rdparty/$setupScript" ];then
-			echo "Setting up $dir/3rdparty"
-			"./$dir/3rdparty/$setupScript"
-		fi
-		echo "/$dir" >> .gitignore
-		;;
-	del)
-		if [[ -z "$2" ]]; then
-			echo "$helpStr"
-			exit 1
-		fi
-		dir=$2
-		echo "Deliting: $dir"
-		if [[ ! -d "$dir" ]]; then
-			echo "Not a dir"
-		fi
-		read -p "Sure (y/n)" conf
-		#TODO check $dir for security (not a "." or "..", doesn't contains slashes)
-		if [[ ! "$conf" = "y" ]];then
-			exit 0;
-		fi
-		rm -rf "$dir"
-		cat .gitignore | sed "/\/$dir/d" > .gitignore
-		;;
-	update)
-		if [[ -z "$2" ]]; then
-			echo "$helpStr"
-			exit 1
-		fi
-		dir=$2
-		if [[ ! -d "$dir" ]]; then
-			echo "Not a dir"
-		fi
+if [[ -z "$1" || "$1" = "all" ]];then
+	for dir in *
+	do
+		if [[ "$dir" = "." || "$dir" = ".." ]];then continue; fi
+		if [[ ! -d "$dir" ]]; then continue; fi
 		echo "Updating: $dir"
 		cd "$dir"
 		git pull
-		cd - > /dev/null
-		;;
-	*)
-		echo "$helpStr"
+		if [ -x "./3rdparty/$updaterScript" ];then
+			echo "Updating $dir/3rdparty"
+			"./3rdparty/$updaterScript" all
+		fi
+	cd - > /dev/null
+	done
+elif [[ "$1" = "add" ]]; then
+	if [[ -z "$2" || -z "$3" ]]; then
+		echo -e "$helpStr";
 		exit 1
-esac
+	fi
+	url=$2
+	dir=$3
+	if [[ -e "$dir" ]]; then
+		echo "Dir $dir already exists"
+		exit 1
+	fi
+	git clone "$url" "$dir"
+	if [ -x "./$dir/3rdparty/$setupScript" ];then
+		echo "Setting up $dir/3rdparty"
+		"./$dir/3rdparty/$setupScript"
+	fi
+	echo "/$dir" >> .gitignore
+elif [[ "$1" = "del" ]];then
+	if [[ -z "$2" ]]; then
+		echo -e "$helpStr"
+		exit 1
+	fi
+	dir=$2
+	echo "Deliting: $dir"
+	if [[ ! -d "$dir" ]]; then
+		echo "Not a dir"
+	fi
+	read -p "Sure (y/n)" conf
+	#TODO check $dir for security (not a "." or "..", doesn't contains slashes)
+	if [[ ! "$conf" = "y" ]];then
+		exit 0;
+	fi
+	rm -rf "$dir"
+	cat .gitignore | sed "/\/$dir/d" > .gitignore
+elif [[ "$1" = "update" ]];then
+	if [[ -z "$2" ]]; then
+		echo -e "$helpStr"
+		exit 1
+	fi
+	dir=$2
+	if [[ ! -d "$dir" ]]; then
+		echo "Not a dir"
+	fi
+	echo "Updating: $dir"
+	cd "$dir"
+	git pull
+	cd - > /dev/null
+else
+	echo -e "$helpStr"
+	exit 1
+fi
 
 
 #if [[ ! -d "./QtUnit" ]]
