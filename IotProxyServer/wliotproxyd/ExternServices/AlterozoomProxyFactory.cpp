55884a5cbdd88a7904091ef45f76ccc04cc38b14
master1312 <master1312@yandex.ru>
2019-02-27 19:08:01 +0300

    fixes

--- IotProxyServer/wliotproxyd/ExternServices/AlterozoomProxyFactory.cpp
+++ IotProxyServer/wliotproxyd/ExternServices/AlterozoomProxyFactory.cpp
@@ -3,7 +3,7 @@
 
 QList<QNetworkProxy> AlteroozomProxyFactory::queryProxy(const QNetworkProxyQuery &query)
 {
-	qDebug()<<"PROXY QUERY: "<<query.peerHostName();
+//	qDebug()<<"PROXY QUERY: "<<query.peerHostName();
 	QList<QNetworkProxy> retVal;
 	if(proxyMap.contains(query.peerHostName()))
 		retVal.append(proxyMap[query.peerHostName()]);
