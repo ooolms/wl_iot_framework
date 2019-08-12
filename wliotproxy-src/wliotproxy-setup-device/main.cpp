4d656f915d3deb4b26292ddebaf6267f3ef0ab2c
master1312 <master1312@yandex.ru>
2019-08-12 21:22:49 +0300

    * wliotproxy-setup-device finished and added to installation

--- wliotproxy-src/wliotproxy-setup-device/main.cpp
+++ wliotproxy-src/wliotproxy-setup-device/main.cpp
@@ -5,23 +5,39 @@
 #include "wliot/devices/CommandCall.h"
 #include <iostream>
 
+const char *helpStr=
+"Usage:\nwliotproxy-setup-device <ttyPort> <newName> [--uuid=newId]\n\n"
+"Example:\nwliotproxy-setup-device ttyACM0 some_device_1\n"
+"or: \nwliotproxy-setup-device ttyACM0 some_device_1 --uuid=033960db747a49c8b3cdc5c82a18c294\nto specify device id";
+
 int main(int argc,char *argv[])
 {
 	QCoreApplication app(argc,argv);
 	CmdArgParser parser(app.arguments());
-	if(parser.args.count()<3||parser.vars.contains("help"))
+	if(parser.args.count()<2||parser.vars.contains("help"))
 	{
-		std::cout<<"Usage:\nwliotproxy-setup-device <ttyPort> <newId> <newName>\n\n";
-		std::cout<<"Example:\nwliotproxy-setup-device ttyACM0 0947ca44cf1c4b17a307574d75da574b some_device_1\n";
+		std::cout<<helpStr<<std::endl;
 		return 0;
+	}
+	QUuid uid=QUuid::createUuid();
+	if(parser.vars.contains("uuid"))
+		uid=QUuid(parser.getVarSingle("uuid"));
+	if(uid.isNull())
+	{
+		std::cerr<<"ERROR: null id specified!"<<std::endl;
+		return 1;
 	}
 	SerialDevice dev(parser.args[0]);
 	if(dev.identify()==RealDevice::FAILED)
 	{
-		std::cerr<<"Not a valid device";
+		std::cerr<<"ERROR: not a compatible device"<<std::endl;
 		return 1;
 	}
-	if(!dev.execCommand("#setup",QByteArrayList()<<parser.args[1].toUtf8()<<parser.args[2].toUtf8())->wait())
+	if(!dev.execCommand("#setup",QByteArrayList()<<uid.toByteArray()<<parser.args[1].toUtf8())->wait())
+	{
+		std::cerr<<"ERROR: setup failed on the device"<<std::endl;
 		return 1;
+	}
+	std::cout<<"OK"<<std::endl;
 	return 0;
 }
--- wliotproxy-src/wliotproxy-setup-device/wliotproxy-setup-device.qbs
+++ wliotproxy-src/wliotproxy-setup-device/wliotproxy-setup-device.qbs
@@ -6,6 +6,14 @@ CppApplication
 	Depends {name: "libwliotproxyd"}
 	cpp.includePaths: ["/usr/include","../wliotproxyd/"]
 
+	Group
+	{
+		name: "wliotproxy-setup-device app"
+		fileTagsFilter: "application"
+		qbs.install: true
+		qbs.installDir: "usr/bin"
+	}
+
 	files:[
         "main.cpp",
 		"../wliotproxyd/CmdArgParser.cpp",
