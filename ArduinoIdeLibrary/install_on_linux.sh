e3aafccf5afbcf3e703bf6beb310f15eeadd37d3
Master1312 <master1312@yandex.ru>
2019-11-20 19:00:27 +0300

    * fixes

--- ArduinoIdeLibrary/install_on_linux.sh
+++ ArduinoIdeLibrary/install_on_linux.sh
@@ -4,4 +4,6 @@ saveD=$PWD
 cd `dirname "$0"`
 rm -rf "$HOME/Arduino/libraries/ARpc"
 cp -R ./ARpc "$HOME/Arduino/libraries"
+rm -rf "$HOME/Arduino/libraries/ARpcESP8266WiFiDevice"
+cp -R ./ARpcESP8266WiFiDevice "$HOME/Arduino/libraries"
 cd "$saveD"
